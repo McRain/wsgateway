03bb2023f062c9d388be542054821fbd002caf17
McRain <admin@reneos.com>
2016-09-22 19:40:30 +0300

    Add GatewayManager

---
+++ GatewayManager.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+const WsGateway = require("./WsGateway.js");
+class GatewayManager {
+    constructor() {
+        this._gateway = null;
+    }
+    static get Handlers() {
+        return this._handlers;
+    }
+    static RegHandler(code, target) {
+        target.Code = code;
+        GatewayManager.Handlers[code] = target.RequestHandler;
+    }
+    static Start(port, origin, userAuth, onConnectHook) {
+        this._gateway = new WsGateway({ "origin": origin, "port": port, "hashKey": "hash=" }, GatewayManager.BasicAuth, userAuth);
+        this._gateway.on(WsGateway.START, function (w) {
+
+        });
+        this._gateway.on(WsGateway.ERROR, function (e) {
+
+        });
+        this._gateway.on(WsGateway.CLOSE, function (c, m, user) {
+
+        });
+        this._gateway.on(WsGateway.CONNECT, onConnectHook);
+        this._gateway.on(WsGateway.MESSAGE, GatewayManager.CallHandler);
+        this._gateway.start();
+    }
+    static BasicAuth(info, done) {
+        return done(true);
+    }
+    static CallHandler(user, target, method, packet, data) {
+
+    }
+}
+module.exports = GatewayManager;
